decff14a2a83e3b2ea8395db71db17ed32957cf2
Brenno Lima <brennolima55@outlook.com>
2023-08-30 23:57:27 -0300

    Ajuste tela de lista de usuarios

--- src/main/resources/JavaScript/ListaDeUsuarios.js
+++ src/main/resources/JavaScript/ListaDeUsuarios.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const userList = document.getElementById("userList");
 
     const users = [
-      { nome: "Usuário 1", email: "usuario1@example.com", status: "Ativo", grupo: "Grupo A", editar:"Editar" },
-      { nome: "Usuário 2", email: "usuario2@example.com", status: "Inativo", grupo: "Grupo B", editar:"Editar" },
+      { nome: "Usuário 1", email: "usuario1@example.com",status: "Ativo" ,grupo: "Grupo A", editar:"Editar" },
+      { nome: "Usuário 2", email: "usuario2@example.com",status: "Ativo" ,grupo: "Grupo B", editar:"Editar" },
       // Adicione mais objetos de usuário conforme necessário
     ];
 
@@ -12,7 +12,11 @@ document.addEventListener("DOMContentLoaded", function () {
       row.innerHTML = `
         <td>${user.nome}</td>
         <td>${user.email}</td>
-        <td>${user.status}</td>
+        <td><div class="switch__container">
+              <input id="switch-shadow" class="switch switch--shadow" type="checkbox">
+              <label for="switch-shadow"></label>
+            </div>
+        </td>
         <td>${user.grupo}</td>
         <td>${user.editar}</td>
 
