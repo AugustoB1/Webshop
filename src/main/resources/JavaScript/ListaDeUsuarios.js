37920fa60671ab24a64acce875d44e56fb4583d4
Brenno Lima <brennolima55@outlook.com>
2023-09-01 10:41:58 -0300

    Adicionando interruptor na tela de lista de usuarios

--- src/main/resources/JavaScript/ListaDeUsuarios.js
+++ src/main/resources/JavaScript/ListaDeUsuarios.js
@@ -2,8 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const userList = document.getElementById("userList");
 
     const users = [
-      { nome: "Usuário 1", email: "usuario1@example.com",status: "Ativo" ,grupo: "Grupo A", editar:"Editar" },
-      { nome: "Usuário 2", email: "usuario2@example.com",status: "Ativo" ,grupo: "Grupo B", editar:"Editar" },
+      { nome: "Usuário 1", email: "usuario1@example.com",status: true ,grupo: "Grupo A", editar:"Editar" },
+      { nome: "Usuário 2", email: "usuario2@example.com",status: true ,grupo: "Grupo B", editar:"Editar" },
+      { nome: "Usuário 3", email: "usuario3@example.com",status: false ,grupo: "Grupo C", editar:"Editar" }
+
       // Adicione mais objetos de usuário conforme necessário
     ];
 
@@ -12,11 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
       row.innerHTML = `
         <td>${user.nome}</td>
         <td>${user.email}</td>
-        <td><div class="switch__container">
-              <input id="switch-shadow" class="switch switch--shadow" type="checkbox">
-              <label for="switch-shadow"></label>
-            </div>
-        </td>
+        <td><label class="switch"><input type="checkbox" ${user.status ? 'checked' : ''}><span class="slider round"></span></label></td>
         <td>${user.grupo}</td>
         <td>${user.editar}</td>
 
